b73dfe7fcb11d48917caac87fcca2235b99380d2
Broflotsky <vinacelo@gmail.com>
2025-05-18 23:46:01 -0500

    feat: Se agrega configuracion de redis para empezar a implementar

--- src/domain/shipments/IShipmentStatusRepository.ts
+++ src/domain/shipments/IShipmentStatusRepository.ts
@@ -29,26 +29,4 @@ export interface IShipmentStatusRepository {
       user_name?: string;
     }>
   >;
-
-  getLatestStatus(shipmentId: number): Promise<{
-    id: number;
-    shipment_id: number;
-    status: string;
-    comment?: string;
-    timestamp: Date;
-    created_by: number;
-    user_name?: string;
-  }>;
-
-  getStatusHistory(shipmentId: number): Promise<
-    Array<{
-      id: number;
-      shipment_id: number;
-      status: string;
-      comment?: string;
-      timestamp: Date;
-      created_by: number;
-      user_name?: string;
-    }>
-  >;
 }
